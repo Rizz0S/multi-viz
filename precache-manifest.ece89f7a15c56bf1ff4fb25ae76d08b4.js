5f73a06b2ec76198dfbe9026f3a064e93ecb8168
Summer Rizzo <srainrizzo@gmail.com>
2020-04-02 20:37:34 -0400

    Updates

---
+++ precache-manifest.ece89f7a15c56bf1ff4fb25ae76d08b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce7cb8625abad2161da38d78cb323b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a128a4157aaa116ada",
+    "url": "/static/css/main.d10e78f2.chunk.css"
+  },
+  {
+    "revision": "7f19e94803e4a35d385a",
+    "url": "/static/js/2.866ee4e7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.866ee4e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a128a4157aaa116ada",
+    "url": "/static/js/main.5d5f3a44.chunk.js"
+  },
+  {
+    "revision": "51cd889cefa3b77a23aa",
+    "url": "/static/js/runtime-main.ca442bed.js"
+  },
+  {
+    "revision": "4d264c398745d4e4f4b24e8721ae378f",
+    "url": "/static/media/sound-icon.4d264c39.png"
+  }
+]);
